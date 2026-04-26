4c87db286924c1c45aed7d8426131558cf3ce4d3
Xonxixn <krhoon21212@gmail.com>
2026-04-26 23:12:54 +0900

    fix type import

--- pages/brands.tsx
+++ pages/brands.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { type GetServerSideProps } from 'next'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { getBrands, Brand } from '@/lib/get-brands'
