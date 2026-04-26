eb8df74f70a26f9892c02f2dd1b86f15c9bee14a
Xonxixn <123652887+Xonxixn@users.noreply.github.com>
2026-04-26 17:53:41 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ import { resolveNotionPage } from '@/lib/resolve-notion-page'
 export const getStaticProps = async () => {
   try {
     const props = await resolveNotionPage(domain)
-
     return { props, revalidate: 10 }
   } catch (err) {
     console.error('page error', domain, err)
-
-    // we don't want to publish the error version of this page, so
-    // let next.js know explicitly that incremental SSG failed
-    throw err
+    // 429 에러면 빌드 실패 대신 fallback 반환
+    return {
+      props: {} as PageProps,
+      revalidate: 5
+    }
   }
 }
 
